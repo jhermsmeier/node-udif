f67344f895916a3be777dbba69ed7f230a9e7faa
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-04-06 14:19:57 +0200

    test: Add tests and test images

---
+++ test/data/index.js
@@ -0,0 +1,17 @@
+module.exports = [{
+  filename: 'zlib-compressed.dmg',
+  uncompressedSize: 9400320,
+  dataForkLength: 14241,
+},{
+  filename: 'raw.dmg',
+  uncompressedSize: 2342912,
+  dataForkLength: 1671680,
+},{
+  filename: 'gpt.dmg',
+  uncompressedSize: 9379840,
+  dataForkLength: 14771,
+},{
+  filename: 'encrypted.dmg',
+  uncompressedSize: 9379840,
+  dataForkLength: 14434,
+}]
---
+++ test/udif.js
@@ -0,0 +1,71 @@
+var assert = require( 'assert' )
+var fs = require( 'fs' )
+var path = require( 'path' )
+var UDIF = require( '..' )
+var images = require( './data' )
+
+const DATADIR = path.join( __dirname, 'data' )
+
+context( 'UDIF.getUncompressedSize()', function() {
+  images.forEach( function( data ) {
+    specify( data.filename, function( done ) {
+      UDIF.getUncompressedSize( path.join( DATADIR, data.filename ), function( error, size ) {
+        assert.ifError( error )
+        assert.equal( size, data.uncompressedSize )
+        done()
+      })
+    })
+  })
+})
+
+context( 'UDIF.Image', function() {
+  images.forEach( function( data ) {
+    context( data.filename, function() {
+
+      var filename = path.join( DATADIR, data.filename )
+      var image = null
+
+      specify( 'new UDIF.Image()', function() {
+        image = new UDIF.Image( filename )
+      })
+
+      specify( 'image.open()', function( done ) {
+        image.open( done )
+      })
+
+      specify( 'image.footer.dataForkLength', function() {
+        assert.equal( image.footer.dataForkLength, data.dataForkLength )
+      })
+
+      specify( 'image.getUncompressedSize()', function() {
+        assert.equal( image.getUncompressedSize(), data.uncompressedSize )
+      })
+
+      specify( 'image.close()', function( done ) {
+        image.close( done )
+      })
+
+    })
+  })
+})
+
+context( 'UDIF.ReadStream', function() {
+  images.forEach( function( data ) {
+    specify( data.filename, function( done ) {
+
+      var bytesRead = 0
+
+      UDIF.createReadStream( path.join( DATADIR, data.filename ) )
+        .on( 'error', done )
+        .on( 'data', function( chunk ) {
+          bytesRead += chunk.length
+          chunk = null
+        })
+        .on( 'end', function() {
+          assert.equal( bytesRead, data.uncompressedSize )
+          done()
+        })
+
+    })
+  })
+})
