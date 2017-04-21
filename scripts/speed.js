61517069026111257e0e3b302768bb7bb945561a
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-04-22 00:07:28 +0200

    chore(scripts): Improve scripts/speed output

--- scripts/speed.js
+++ scripts/speed.js
@@ -37,16 +37,30 @@ var ERASELINE = CURSOR_UP + CURSOR_LEFT + ERASE_LINE
 image.open(( error ) => {
   if( error ) throw error
   console.log( inspect( image ) )
+  process.stdout.write( '\n\n' )
+  console.log( '  compressed size:', bytes(image.footer.dataForkLength) )
+  console.log( 'uncompressed size:', bytes(image.getUncompressedSize()) )
+  process.stdout.write( '\n' )
   image.close(( error ) => {
     if( error ) throw error
     var start = Date.now()
-    process.stdout.write( '\n\n\n\n' )
+    var first = true
     UDIF.createReadStream( filename )
       .on( 'data', function() {
         var time = ( Date.now() - start ) / 1000
+        if( first ) {
+          process.stdout.write( '\n\n' )
+          first = false
+        }
         process.stdout.write( ERASELINE )
         process.stdout.write( `Bytes read: ${bytes(this.bytesRead)} (${bytes((this.bytesRead/time)|0)}/s)\n` )
         process.stdout.write( `Bytes written: ${bytes(this.bytesWritten)} (${bytes((this.bytesWritten/time)|0)}/s)\n` )
+      })
+      .on( 'end', function() {
+        process.stdout.write( `Done.\n` )
+      })
+      .on( 'readable', function() {
+        while( this.read() ) continue
       })
   })
 })
